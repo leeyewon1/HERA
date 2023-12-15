30ad8d876148b8844d1b4c57fca93bf5f1292480
leeyewon1 <148739266+leeyewon1@users.noreply.github.com>
2023-12-15 15:15:51 +0900

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,45 @@
+$(function () {
+    $('.main_visual .visual_slide').slick({
+        arrows: false,
+        autoplay: true,
+        pauseOnHover: false,
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
+
+    $('.side_content .slide_wrap').slick({
+        slidesToShow: 4,
+        arrows: false,
+        dots: false,
+        slidesPerGroup: 4,
+
+
+    });
+
+    $('.side_content .arrows .left').on('click', function () {
+        $('.slide_wrap').slick('slickPrev');
+    });
+
+    $('.side_content .arrows .right').on('click', function () {
+        $('.slide_wrap').slick('slickNext');
+    });
+
+    $('.totop').on('click', function () {
+        $('html, body').animate({ scrollTop: 0 }, 800)
+    });
+
+    $(window).on('scroll', function () {
+        const sct = $(window).scrollTop();
+        if (sct > 300) {
+            $('.totop').addClass('on');
+            $('.Header').addClass('on');
+        } else {
+            $('.totop').removeClass('on');
+            $('.Header').removeClass('on');
+        }
+    });
+
+});
